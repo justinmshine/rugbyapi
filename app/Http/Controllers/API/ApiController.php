116749df880d1bde145fd7457e322465756848b0
Justin Shine <justin.m.shine@gmail.com>
2024-11-27 11:40:34 +0000

    Save a sales record in the CMS if its send to the post api endpoint

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\ShirtsModel;
 use App\Models\DimensionsModel;
+use App\Models\SalesModel;
 use Validator;
 use App\Http\Resources\ShirtsResource;
 use Illuminate\Http\JsonResponse;
@@ -33,5 +34,37 @@ class ApiController extends BaseController
         }
 
         return $this->sendResponse(ShirtsResource::collection($shirts), 'Products retrieved successfully.');
+    }
+
+    /**
+     * Store a newly created sale in the database.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'surname' => 'required',
+            'address1' => 'required',
+            'city' => 'required',
+            'country' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'card' => 'required',
+            'expires' => 'required',
+            'cvc' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        SalesModel::create($input);
+
+        return $this->sendResponse($input, 'Sale created successfully.');
     }
 }
---
+++ app/Models/SalesModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalesModel extends Model
+{
+    protected $table = "sales";
+
+    protected $fillable = ['name', 'surname', 'address1', 'address2', 'city', 'country', 'phone', 'email', 'card', 'expires', 'cvc'];
+}
